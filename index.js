c8d251e8cd1ff43f5c2b20c648a3d5ecba7f37d6
Sayan Das <sayan.das@sheshi.ai>
2023-01-01 00:53:43 +0530

    code chenges

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-// const express = require('express');
-// require('dotenv').config()
-// const app = express();
-// const cors=require('cors');
-// const PORT=process.env.PORT ||8090
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const cors=require('cors');
+const PORT=process.env.PORT ||8090
 
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 
 
-// app.get('/',(req,res)=>{
-//     try {
-//         res.json({nice:'ice'})
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
+app.get('/',(req,res)=>{
+    try {
+        res.json({nice:'ice'})
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
-// app.listen(PORT,()=>{
-//     console.log(`listening on post ${PORT}`);
-// })
-
-const http = require('http');
-
-http.createServer(function(req, res){
-    res.write('nice ice');
-    res.end()
-}).listen(80)
+app.listen(PORT,()=>{
+    console.log(`listening on post ${PORT}`);
+})
